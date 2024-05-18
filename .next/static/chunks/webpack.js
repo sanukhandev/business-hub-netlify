b67226f3ca22ef44cfc5c2414613693240efbaf8
Sherin <csherin111@gmail.com>
2024-05-18 18:32:38 +0400

    Logo size updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "058bc7563fa15600"; }
+/******/ 		__webpack_require__.h = function() { return "54f7f0ad5517ee83"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
