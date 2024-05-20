869a4d30fa7f1cfb745913d4c7edf4186ff1c336
Sherin <csherin111@gmail.com>
2024-05-20 22:50:34 +0400

    colors updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee94dc9e8bd3544e"; }
+/******/ 		__webpack_require__.h = function() { return "60977eefdac5a0e8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
