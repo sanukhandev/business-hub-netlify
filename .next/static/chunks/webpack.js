09350b1e019e753eb140ef5ac4d4b73d05ed27b9
Sherin <csherin111@gmail.com>
2024-06-02 00:28:24 +0400

    Introvideo update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fc7d00f3c338b39"; }
+/******/ 		__webpack_require__.h = function() { return "1f91fb0d84c6b3bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Intro4/intro4.jsx
+++ src/components/Intro4/intro4.jsx
@@ -3,24 +3,21 @@ import particlesConfig from "../../config/particle-config";
 import particlesBlackConfig from "../../config/pr-s-black";
 import Particles from "react-tsparticles";
 import VideoPlayer from "react-background-video-player";
-import svg from "./skyline.svg";
+
 const Intro4 = ({ sliderRef, blackStar }) => {
   return (
     <header ref={sliderRef} className="particles circle-bg valign">
       <div className="container">
-      <VideoPlayer
-        className="video"
-        src={
-          "https://res.cloudinary.com/dgwddohoe/video/upload/v1717191605/logo_reveal_zon4nu.mp4"
-        }
-        autoPlay={true}
-        muted={true}
-      />
+        <VideoPlayer
+          className="video"
+          src={
+            "https://res.cloudinary.com/dgwddohoe/video/upload/v1717191605/logo_reveal_zon4nu.mp4"
+          }
+          autoPlay={true}
+          muted={true}
+        />
       </div>
-      <div className="App">
-      
-    
-    </div>
+      <div className="App"></div>
       <Particles
         id="particles-js"
         options={blackStar ? particlesBlackConfig : particlesConfig}
